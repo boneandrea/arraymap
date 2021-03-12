0489ced1beba9ee6b3dcace5362ba7a787b4c5f4
Your Name <you@example.com>
2021-03-12 14:58:46 +0900

    Change menu language

--- define.php
+++ define.php
@@ -7,7 +7,7 @@ $Contact="Contact";
 $AboutUs="About Us";
 
 // ja
-$Introduction="トップ";
-$OurCompany="制作実績";
-$Contact="お問い合わせ";
-$AboutUs="私達について";
+// $Introduction="トップ";
+// $OurCompany="制作実績";
+// $Contact="お問い合わせ";
+// $AboutUs="私達について";
